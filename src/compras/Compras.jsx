015faf1dae0ce5720173fbcd8011b65d746c27af
Anderson Luís Furlan <alsfurlan@gmail.com>
2025-05-05 21:50:18 -0300

    Compras

--- src/compras/Compras.jsx
+++ src/compras/Compras.jsx
@@ -2,11 +2,10 @@ import './Compras.css'
 import { useState } from 'react';
 
 const Compras = () => {
-
+    const [item, setItem] = useState('');
     const [itens, setItens] = useState(['Arroz', 'Feijão', 'Carne', 'Salada']);
 
     const adicionar = () => {
-        const item = 'Frango'; // TODO alterar para o input
         // Jeito errado
         // itens.push(item);
         // setItens(itens);
@@ -16,15 +15,30 @@ const Compras = () => {
             // return itens;
             return [...itens, item]
         });
+        setItem('');
+    }
+
+    const remover = (item) => {
+        setItens((itens) => {
+            return [...itens.filter(i => i !== item)];
+        })
     }
 
     return (
         <section className="compras">
             <h1>Compras</h1>
-            <button onClick={adicionar}>Adicionar</button>
+            <div className="input">
+                <input value={item} onChange={(e) => setItem(e.target.value)} />
+                <button onClick={adicionar}>Adicionar</button>
+            </div>
             <ol>
                 {itens.map((item, indice) => {
-                    return <li key={indice}>{item}</li>
+                    return <li key={indice}>
+                        <button onClick={() => remover(item)}>
+                            X
+                        </button>
+                        <span>{item}</span>
+                    </li>
                 })}
             </ol>
         </section>
